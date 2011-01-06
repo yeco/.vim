08b5c7bc40811fce34cd8a27fa55e494d88feadb
= <wavded@gmail.com>
2011-01-06 12:12:13 -0600

    pulling command-t from repo

--- update.sh
+++ update.sh
@@ -32,6 +32,7 @@ get_repo() {
 get_other_repo() {
    path=$1
    repo=$2
+
    echo "Checking $repo"
    if [ -d "$DOTVIM/bundle/$repo/" ]
    then
@@ -41,7 +42,7 @@ get_other_repo() {
       cd ..
    else
       echo "Cloning repo for $repo"
-      git clone $url$repo
+      git clone $url$repo.git
    fi
 }
 
@@ -98,6 +99,12 @@ get_repo "robgleeson" "vim-markdown-preview"
 echo "Installing vcscommand"
 get_other_repo "git://repo.or.cz/" "vcscommand"
 
+echo "Installing command-t"
+get_other_repo "git://git.wincent.com/" "command-t"
+echo "Building commant-t"
+cd $DOTVIM/bundle/command-t
+rake make
+
 cd $DOTVIM/autoload
 echo "Fetching latest pathogen.vim"
 rm pathogen.vim
@@ -113,33 +120,4 @@ then
 	echo "$pathogen_cmd" >> ~/.vimrc
 else
 	echo "It was already added. Good to go"
-fi
-
-ct=`curl -s https://wincent.com/products/command-t | grep releases | head -1 | cut -d\" -f2`
-cd /tmp
-vba=$( echo "$ct" | ruby -ruri -e 'puts File.basename(gets.chomp)' )
-echo "***********************************************************"
-echo "Would you like to download and install Command-T: $vba? If so, type yes. Anything else will bypass."
-echo "***********************************************************"
-read answer
-if [ "$answer" == 'yes' ]
-then
-    echo "Installing Command-T plugin"
-    curl -O $ct
-
-    echo ""
-    echo ""
-    echo "***********************************************************"
-    echo "Vim will start, then :so %"
-    echo "When the script is done it'll compile the ruby extension"
-    echo "***********************************************************"
-    sleep 3
-    vim /tmp/$vba
-    sleep 3
-    cd ~/.vim/ruby/command-t
-    ruby extconf.rb
-    make
-else
-    echo "Skipping install, just run this script again if you want it"
-    echo "***********************************************************"
 fi
